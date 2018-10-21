24aa6e0b8fab7a2ea2a6ae8e221bb1aa37fcc2e1
Daniel Lidstrom <dlidstrom@gmail.com>
2018-10-21 16:17:59 +0200

    Some prerequisites in place

--- Web.Core/LocalEntryPoint.cs
+++ Web.Core/LocalEntryPoint.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace Web.Core
+﻿namespace Web.Core
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            try
+            {
+                var host = WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
+                var logger = host.Services.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("Starting application");
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+                return 1;
+            }
         }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
     }
 }
