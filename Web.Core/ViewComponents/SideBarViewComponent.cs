0e192b46c7b0847d0e7edae7695e2ee945cd50e5
Daniel Lidstrom <dlidstrom@gmail.com>
2018-10-21 18:45:13 +0200

    View component working

--- Web.Core/ViewComponents/SideBarViewComponent.cs
+++ Web.Core/ViewComponents/SideBarViewComponent.cs
@@ -4,13 +4,14 @@ namespace PrimeNumberCalculator.ViewComponents
     using Microsoft.AspNetCore.Mvc;
     using PrimeNumberCalculator.Model;
 
+    [ViewComponent]
     public class SideBarViewComponent : ViewComponent
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var urls = await Task.FromResult(new []
             {
-                new Link { Url = "snittlistan.se", Text = "Snittlistan" }
+                new Link { Url = "https://snittlistan.se", Text = "Snittlistan" }
             });
 
             return View(urls);
