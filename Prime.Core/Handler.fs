67d564252e6a041f1ccd34cdfe28019154292a5f
Daniel Lidstrom <dlidstrom@gmail.com>
2018-11-13 20:52:27 +0100

    Fixes #3

--- Prime.Core/Handler.fs
+++ Prime.Core/Handler.fs
@@ -8,10 +8,6 @@ type Request = { Key1 : string; Key2 : string; Key3 : string }
 type Response = { Message : string; Request : Request }
 
 module Handler =
-    open System
-    open System.IO
-    open System.Text
-
     let hello (request : Request) =
         {
             Message = "Go Serverless v1.0! Your function executed successfully!"
